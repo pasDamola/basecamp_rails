499a2129cbf77a8b5a66075962e11bc56ad08c03
pasDamola <oyindadavid18@gmail.com>
2020-01-10 05:05:35 -0800

    basecamp 1

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,4 +1,6 @@
 class Idea < ApplicationRecord
+    validates_presence_of   :name, :message => 'Please Enter Name Of Project.'
+    validates_presence_of   :description, :message => 'Please Enter Description of  Project.'
     belongs_to :user
     has_many :comments
 end
