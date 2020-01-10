bb8b9c742431ef8c5594b917a60777c79a916aea
pasDamola <oyindadavid18@gmail.com>
2020-01-10 02:51:46 -0800

    active admin dashboard set

---
+++ app/admin/ideas.rb
@@ -0,0 +1,19 @@
+ActiveAdmin.register Idea do
+  permit_params :name, :description, :picture
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :name, :description, :picture, :user_id
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:name, :description, :picture, :user_id]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
